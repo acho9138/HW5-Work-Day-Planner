baf1b0a4e2e35f986e29229a0a70f1948733c733
Anna C <anna.s.chong@gmail.com>
2020-06-20 14:22:11 +1000

    ADD Colour to Timeblock Dependent on Time (grey for past, red for present and green for future)

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // Display current date
-const currentDate = moment().format('dddd, MMMM Do, YYYY')
-$("#currentDay").text(currentDate)
+const currentDate = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").text(currentDate);
+
+// Display timeblocks in colours according to past, present or future
+const currentHour = moment().hours();
+const timeBlockArray = $('.timeBlock').children();
+
+timeBlockArray.each((index, value) => {
+    const blockHour = $(value).attr('data-hour');
+    if (currentHour > blockHour) {
+        $(value).find('.description').addClass('past')
+    } else if (currentHour < blockHour) {
+        $(value).find('.description').addClass('future')
+    } else {
+        $(value).find('.description').addClass('present')
+    }
+})
