7db54aa245dd87f31e3f37f159f9bf0f7fec38ff
Anna C <anna.s.chong@gmail.com>
2020-06-21 14:00:16 +1000

    ADD User Input into LocalStorage & Display When Page Reloads

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ $("#currentDay").text(currentDate);
 
 // Display timeblocks in colours according to past, present or future
 const currentHour = moment().hours();
-const timeBlockArray = $('.timeBlock').children();
+const timeBlockArray = $(".timeBlock").children();
 
 timeBlockArray.each((index, value) => {
-    const blockHour = $(value).attr('data-hour');
-    if (currentHour > blockHour) {
-        $(value).find('.description').addClass('past')
-    } else if (currentHour < blockHour) {
-        $(value).find('.description').addClass('future')
-    } else {
-        $(value).find('.description').addClass('present')
-    }
-})
+  const blockHour = $(value).attr("data-hour");
+  if (currentHour > blockHour) {
+    $(value).find(".description").addClass("past");
+  } else if (currentHour < blockHour) {
+    $(value).find(".description").addClass("future");
+  } else {
+    $(value).find(".description").addClass("present");
+  }
+});
+
+// Get user input of description and put in local storage
+$(".button").on("click", (event) => {
+  const id = $(event.currentTarget.parentElement).parent().attr("data-hour");
+  const userInput = $(event.currentTarget.parentElement).parent().find(".description").val();
+  localStorage.setItem(id, userInput);
+});
+
+// Display saved description from local storage after refreshing
+timeBlockArray.each((index, value) => {
+    const blockHour = $(value).attr("data-hour");
+    const storedDescription = localStorage.getItem(blockHour)
+    $(value).find(".description").val(storedDescription)
+});
